6f13d274169c19f2ad9ecbec7e126b867e21e09f
Tejeswara Sai Appikatla <appikatlahome@gmail.com>
2021-02-01 12:51:40 +0530

    Implemented Everything in Calculator.java Class as Instructed

--- src/main/java/EPAM/HomeTask1/Calculator.java
+++ src/main/java/EPAM/HomeTask1/Calculator.java
@@ -3,6 +3,31 @@ package EPAM.HomeTask1;
 import java.util.Scanner;
 public class Calculator 
 {
+	public static void Add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	
+	public static void Divide(int a,int b)
+	{
+		if(b==0)
+		{
+			System.out.println("Divide With Zero");
+		}
+		else
+		{
+			System.out.println(a/b);
+		}
+	}
+	public static void Multiply(int a,int b)
+	{
+		System.out.println(a*b);
+	}
+	public static void Subtract(int a, int b)
+	{
+		System.out.println(a-b);
+	}
+	
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
@@ -17,19 +42,38 @@ public class Calculator
 				+ "Subtraction (a-b) - 2\n"
 				+ "Multiplication (a*b) - 3\n"
 				+ "Division (a/b) - 4");
-		int operation = scan.nextInt();
-		System.out.println(operation);
-		Operation Op = new Operation();
-		int output = Op.PerformOperation(a, b, operation);
-		if(output == -2)
-		{
-			System.out.println("Enter Valid Operations");
-		}
-		else
-		{
-			System.out.println("Result of the Desired Operation on the Provided Numbers is"+output);
-		}
+		int op = scan.nextInt();
+		System.out.println(op);
 		
+			if(op == 1)
+			{
+				System.out.println("Performing Addition Operation on "+a+" "+b);
+				Add(a,b);
+			}
+			else if(op == 2)
+			{
+				System.out.println("Performing Subtraction Operation on "+a+" "+b);
+				if(a>=b)
+				{
+					Subtract(a,b);
+				}
+				else
+				{
+					Subtract(b,a);
+				}
+			
+			}
+			else if(op == 3)
+			{
+				System.out.println("Performing Multiplication Operation on "+a+" "+b);
+				Multiply(a,b);
+			
+			}
+			else if(op == 4)
+			{
+				System.out.println("Performing Division Operation on "+a+" "+b);
+				Divide(a,b);
+			}
 		scan.close();
 		
 	}
