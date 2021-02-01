a788a9a3d4b19b460725c10ea73175e7fd2e4a00
Tejeswara Sai Appikatla <appikatlahome@gmail.com>
2021-02-01 12:18:41 +0530

    Written the Base Class Calculator.java with the input and created a Class for Performing Operations

--- src/main/java/EPAM/HomeTask1/Calculator.java
+++ src/main/java/EPAM/HomeTask1/Calculator.java
@@ -1,7 +1,25 @@
 package EPAM.HomeTask1;
 
+import java.util.Scanner;
 public class Calculator 
 {
-	int
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Two Numbers on which you want to perform operations");
+		int a,b;
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println("Enter the operation that you want to perform on the inputted Numbers\n"
+				+ "Addition - 1\n"
+				+ "Subtraction - 2\n"
+				+ "Multiplication - 3\n"
+				+ "Division - 4");
+		int operation = scan.nextInt();
+		System.out.println(operation);
+		
+	}
 
 }
